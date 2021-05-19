ad13ef6017751fc26d373f6fde8db74b54c0c9eb
Maurits Brouwer <36409024+Maurits97@users.noreply.github.com>
2021-05-19 17:08:02 +0200

    added responsiveness + mobile navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,20 @@ import styles from '../styles/Navbar.module.scss'
 const Navbar = () => {
   const router = useRouter();
   
+  let mobileNavOpen = false;
+
+  function navClicked() {
+    const mobileNav = document.getElementById('mobile__nav');
+
+    if (mobileNavOpen === false) {
+      mobileNav.classList.add(`${styles.mobile__active}`) //adds active class
+      mobileNavOpen = true;
+    } else {
+      mobileNav.classList.remove(`${styles.mobile__active}`) //removes active class
+      mobileNavOpen = false;
+    }
+  }
+  
   return ( 
     <nav className={styles.nav}>
       <Link href="/"><img className={styles.logo} src="/img/Fivides_Logo_Luc_Brouwer.png" /></Link>
@@ -15,6 +29,22 @@ const Navbar = () => {
         <Link href="/"><a>Analysemodel</a></Link>
         <Link href="/"><a>Rapportage</a></Link>
         <Link href="/"><a>Over ons</a></Link>
+      </div>
+
+      <div className={styles.hamburger} onClick={navClicked}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+      </div>
+
+      <div className={styles.mobile__nav} id="mobile__nav">
+        <svg onClick={navClicked} className={styles.cross} xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
+
+        <div className={styles.mobile__nav__items}>
+          <Link href="/"><a onClick={navClicked} className={router.pathname == "/" ? styles.items__active : ""}>Home</a></Link>
+          <Link href="/actiepad"><a onClick={navClicked} className={router.pathname == "/actiepad" ? styles.items__active : ""}>Actiepad</a></Link>
+          <Link href="/"><a>Analysemodel</a></Link>
+          <Link href="/"><a>Rapportage</a></Link>
+          <Link href="/"><a>Over ons</a></Link>
+        </div>
       </div>
     </nav>
    );
--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
         <h3 className="contact__header">Nu met <span className="green">50%</span> introductiekorting </h3>
         <p className="contact__text line-height">Ter introductie is de Fivides Actiepad Analyse, met een rapportage in power point, nu te bestellen met 50% korting. Je betaalt slechts €475 (excl. 21% BTW) in plaats van €950 (excl. 21% BTW). Binnen vijf werkdagen na aanvraag en betaling wordt de rapportage inclusief de power point toegestuurd.</p>
         <h4 className="contact__subheader">Interesse?</h4>
-        <p className="line-height">Mail nu Luc Brouwer van Fivides om een Actiepad Analyse met 50% korting te bestellen.</p>
+        <p className="contact__subtext line-height">Mail nu Luc Brouwer van Fivides om een Actiepad Analyse met 50% korting te bestellen.</p>
         <a href="mailto:l.brouwer@fivides.nl" className="contact__email" target="_blank">l.brouwer@fivides.nl</a>
       </div>  
     </div>
